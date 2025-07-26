328f966a50518ad8355e8fd09b1c04c0d27b94b5
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-26 15:34:07 +0300

    Update Copy.ts

--- hw01/src/models/Copy.ts
+++ hw01/src/models/Copy.ts
@@ -1,3 +1,21 @@
-export class Copy {
-  // TODO
+import { AbstractBook } from "./AbstractBook";
+import { Author } from "./Author";
+
+export class Book extends AbstractBook {
+  constructor(
+    title: string,
+    year: number,
+    private _author: Author
+  ) {
+    super(title, year);
+    this._author.addBook(this);
+  }
+
+  get author(): Author {
+    return this._author;
+  }
+
+  getDescription(): string {
+    return `Physical book "${this.title}" by ${this.author.name} (${this.year})`;
+  }
 }
