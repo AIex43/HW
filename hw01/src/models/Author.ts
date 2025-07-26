d115c3115d290b6ecbd24efd752b8956d9363cca
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-26 15:32:47 +0300

    Update Author.ts

--- hw01/src/models/Author.ts
+++ hw01/src/models/Author.ts
@@ -1,3 +1,19 @@
+import { AbstractBook } from "./AbstractBook";
+
 export class Author {
-  // TODO
+  private _books: AbstractBook[] = [];
+
+  constructor(private _name: string) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  get books(): readonly AbstractBook[] {
+    return this._books;
+  }
+
+  addBook(book: AbstractBook): void {
+    this._books.push(book);
+  }
 }
