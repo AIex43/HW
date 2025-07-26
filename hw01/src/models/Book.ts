6937324560e6cd7a0011099783286ac065e1e361
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-26 15:33:53 +0300

    Update Book.ts

--- hw01/src/models/Book.ts
+++ hw01/src/models/Book.ts
@@ -1,5 +1,21 @@
+import { AbstractBook } from "./AbstractBook";
 import { Author } from "./Author";
 
-export class Book {
-  // TODO
+export class Book extends AbstractBook {
+  constructor(
+    title: string,
+    year: number,
+    private _author: Author
+  ) {
+    super(title, year);
+    this._author.addBook(this);
+  }
+
+  get author(): Author {
+    return this._author;
+  }
+
+  getDescription(): string {
+    return `Physical book "${this.title}" by ${this.author.name} (${this.year})`;
+  }
 }
