c65fd995520d0bccbe2148c66d59f5e0d58dee7a
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-26 15:35:26 +0300

    Update Reader.ts

--- hw01/src/models/Reader.ts
+++ hw01/src/models/Reader.ts
@@ -1,5 +1,38 @@
 import { Copy } from "./Copy";
 
 export class Reader {
-  // TODO
+  private _borrowed: Copy[] = [];
+
+  constructor(
+    private _id: string,
+    private _name: string
+  ) {}
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get borrowedCopies(): readonly Copy[] {
+    return this._borrowed;
+  }
+
+  borrowCopy(copy: Copy): boolean {
+    if (copy.borrow()) {
+      this._borrowed.push(copy);
+      return true;
+    }
+    return false;
+  }
+
+  returnCopy(copy: Copy): void {
+    const idx = this._borrowed.indexOf(copy);
+    if (idx >= 0) {
+      this._borrowed.splice(idx, 1);
+      copy.return();
+    }
+  }
 }
