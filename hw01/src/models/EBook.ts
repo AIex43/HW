421e27258f241a69e4d0ffb0cd75d44f64338336
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-26 15:34:21 +0300

    Update EBook.ts

--- hw01/src/models/EBook.ts
+++ hw01/src/models/EBook.ts
@@ -1,3 +1,26 @@
-export class EBook {
-  // TODO
+import { AbstractBook } from "./AbstractBook";
+import { Author } from "./Author";
+
+export class EBook extends AbstractBook {
+  constructor(
+    title: string,
+    year: number,
+    private _author: Author,
+    private _url: string
+  ) {
+    super(title, year);
+    this._author.addBook(this);
+  }
+
+  get author(): Author {
+    return this._author;
+  }
+
+  get url(): string {
+    return this._url;
+  }
+
+  getDescription(): string {
+    return `E-book "${this.title}" by ${this.author.name} (${this.year}) - Available at: ${this.url}`;
+  }
 }
