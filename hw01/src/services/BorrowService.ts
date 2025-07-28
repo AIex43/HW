ded2f1dee5283e010e9dbaf2d7db02600a327670
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-07-28 12:31:50 +0300

    Update BorrowService.ts

--- hw01/src/services/BorrowService.ts
+++ hw01/src/services/BorrowService.ts
@@ -2,5 +2,29 @@ import { Reader } from "../models/Reader";
 import { Copy } from "../models/Copy";
 
 export class BorrowService {
-  // TODO
+
+  borrow(reader: Reader, copy: Copy): void {
+    if (!copy.getAvailability()) {
+      throw new Error("Цей примірник уже позичений!");
+    }
+
+
+    copy.borrow();
+
+
+    reader.borrowCopy(copy);
+  }
+
+
+  return(reader: Reader, copy: Copy): void {
+
+    copy.return();
+
+
+    const borrowed = reader.getBorrowedCopies();
+    const index = borrowed.indexOf(copy);
+    if (index !== -1) {
+      borrowed.splice(index, 1);
+    }
+  }
 }
